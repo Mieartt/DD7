7c62382dba82e59a2f325d51b96ac984b279534d
Mieartt <ard.iheb@gmail.com>
2025-02-04 20:58:47 +0100

    Intial commit

---
+++ salah.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity salah is
+ port (
+        fom    :      in std_logic;
+        khcham :      out std_logic
+ );
+ end entity;
+ architecture behavior of salah is 
+ begin
+ end behavior;
+
+ 
