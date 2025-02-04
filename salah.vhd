03ecc16f72ab776983f02931f89cbb99f847e1ee
Mieartt <ard.iheb@gmail.com>
2025-02-04 21:02:16 +0100

    second commit

--- salah.vhd
+++ salah.vhd
@@ -2,11 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity salah is
  port (
-        fom    :      in std_logic;
-        khcham :      out std_logic
+        fom    : in std_logic;
+        khcham : out std_logic
  );
  end entity;
- architecture behavior of salah is 
+ architecture arch of salah is 
  begin
  end behavior;
 
